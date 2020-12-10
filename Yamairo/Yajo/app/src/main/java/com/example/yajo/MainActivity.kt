61141c71b79f8d2a417ed382e50fd87583c2a427
Yamairo Heshuys <341116@talnet.nl>
2020-12-10 21:09:05 +0100

    Yajo

---
+++ Yamairo/Yajo/app/src/main/java/com/example/yajo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.yajo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Spinner
+
+
+public class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+}
